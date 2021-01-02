0737754a91719ef59b91ac655762e83a36d90222
Sarbe <sarbe97@gmail.com>
2021-01-02 21:57:38 +0530

    initial

--- src/main.js
+++ src/main.js
@@ -2,11 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import * as firebase from 'firebase'
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App),
+    created() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyDocq3LnlChMnMwNki9U7x3BfZFNFQvDYc",
+            authDomain: "onelocker-vue.firebaseapp.com",
+            databaseURL: "https://onelocker-vue.firebaseio.com",
+            projectId: "onelocker-vue",
+            storageBucket: "onelocker-vue.appspot.com"
+        })
+    }
 }).$mount('#app')
